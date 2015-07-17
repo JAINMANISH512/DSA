327e9d27120091d8f2279fa63f2a6cd08db98df0
Manish Jain <jain_manish512@yahoo.com>
2015-07-18 01:43:03 +0530

    Day 6 -Graph and Stack

--- Graph.py
+++ Graph.py
@@ -1,11 +1,12 @@
-from collections import deque
+
 class adjnode(object):
-    def __init__(self,dest,next=None):
+    def __init__(self,dest,weight=0,next=None):
         self.dest=dest
         self.next=next
+        self.weight=weight
         
     def __str__(self):
-        return str(self.dest)
+        return str(self.dest)+str(self.weight)
 
 class adjlist(object):
     def __init__(self,head=None,tail=None):
@@ -29,6 +30,20 @@ class graph(object):
 
     def __str__(self):
         return str(self.array)
+class edge(object):
+    def __init__(self,src,dest):
+        self.src=src
+        self.dest=dest
+    def __str__(self):
+        return str(self.src)+"---"+str(self.dest)
+class newgraph(object):
+    def __init__(self,v,e):
+        self.v=v
+        self.e=e
+        self.edgelist=[]
+        for i in range(self.e):
+            self.edgelist.append(edge(None,None))
+
 class queue(object):
     def __init__(self,front=None,back=None):
 		self.front=None
@@ -69,18 +84,18 @@ def printgraph(g):
         print str(src)+ "->"+str(g.array[src])
         
 
-def addedge(g,src,dest):
+def addedge(g,src,dest,weight=0):
 	#print str(src)+"->"+str(dest)+"added"
-	new=adjnode(dest,g.array[src].head)
+	new=adjnode(dest,weight,g.array[src].head)
 	g.array[src].head=new
 	return g
-def adduedge(g,src,dest):
-	addedge(g,src,dest)
-	addedge(g,dest,src)
+def adduedge(g,src,dest,weight=0):
+	addedge(g,src,dest,weight)
+	addedge(g,dest,src,weight)
 	return g
-def addedgeend(g,src,dest):
+def addedgeend(g,src,dest,weight=0):
 	#print str(src)+"->"+str(dest)+" added"
-	new=adjnode(dest)
+	new=adjnode(dest,weight)
 	if g.array[src].tail==None:
 		g.array[src].head=g.array[src].tail=new
 	else:
@@ -88,9 +103,9 @@ def addedgeend(g,src,dest):
 		g.array[src].tail=new
 	return g
 
-def adduedgeend(g,src,dest):
-	addedgeend(g,src,dest)
-	addedgeend(g,dest,src)
+def adduedgeend(g,src,dest,weight=0):
+	addedgeend(g,src,dest,weight)
+	addedgeend(g,dest,src,weight)
 	return g
 
 def bfs(g,src):
@@ -101,7 +116,7 @@ def bfs(g,src):
     myqueue=queue()
     myqueue.enq(src)
     while myqueue.isemp()==False:
-        
+
         s=myqueue.deq()
         print s,
         curr=g.array[s].head
@@ -110,26 +125,233 @@ def bfs(g,src):
                 myqueue.enq(curr.dest)
                 visited[curr.dest]=True
             curr=curr.next   
+def dfs(g):
+    visited=[]
+    for i in range(g.v):
+        visited.append(False)
+    for i in range(g.v):
+        if not visited[i]:
+            visited=dfsutil(i,g,visited)
+            
+        
+    #visited=dfsutil(3,g,visited)
+def dfsutil(src,g,visited):
+    visited[src]=True
+    print src,
+    curr=g.array[src].head
+    
+    while curr:
+        if not visited[curr.dest]:
+            visited=dfsutil(curr.dest,g,visited)
+        
+        curr=curr.next
+    
+    return visited
+
+def dfscycle(g):
+    visited=[]
+    for i in range(g.v):
+        visited.append(False)
+    for i in range(g.v):
+        #print "DFS from "+str(i)
+        if not visited[i]:
+            visited,status=dfsutilcycle(i,g,visited)
+            if status==1:
+                return
+        
+    
+def dfsutilcycle(src,g,visited):
+    visited[src]=True
+    status=0
+    
+    curr=g.array[src].head
+    
+    while curr:
+        if not visited[curr.dest]:
+            visited,status=dfsutilcycle(curr.dest,g,visited)
+            if status==1:
+                #print src,visited,status
+                break
+
+        else:
+            #print "src with cycle is "+str(src) +"to "+str(curr)
+            print "A cycle detected "
+            status=1
+            break
+        curr=curr.next
+    visited[src]=False
+    return visited,status
+
+def find(parent,i):
+    if parent[i]==-1:
+        return i
+    else:
+        return find(parent,parent[i])
+
+def union(parent,x,y):
+    xset=find(parent,x)
+    yset=find(parent,y)
+    parent[xset]=yset
+
+def isundircycle(g):
+    parent=[]
+    for i in range(g.v):
+        parent.append(-1)
+    for i in range(g.e):
+        x=find(parent,g.edgelist[i].src)
+        y=find(parent,g.edgelist[i].dest)
+        if x==y:
+            return True
+        union(parent,x,y)
+
+    return False
+
+def undirecdfscycle(g):
+    visited=[]
+    for i in range(g.v):
+        visited.append(False)
+    for i in range(g.v):
+        #print "DFS from "+str(i)
+        if not visited[i]:
+            visited,status=undirecddfsutilcycle(i,g,visited,-1)
+            if status==1:
+                return True
+    return False
+        
+    
+def undirecddfsutilcycle(src,g,visited,parent):
+    visited[src]=True
+    status=0
+    
+    curr=g.array[src].head
+    
+    while curr:
+        if not visited[curr.dest]:
+            visited,status=undirecddfsutilcycle(curr.dest,g,visited)
+            if status==1:
+                #print src,visited,status
+                break
+
+        else:
+            #print "src with cycle is "+str(src) +"to "+str(curr)
+            print "A cycle detected "
+            status=1
+            break
+        curr=curr.next
+    visited[src]=False
+    return visited,status
+
+def topo(g):
+    visited=[]
+    stack=[]
+    for i in range(g.v):
+        visited.append(False)
+    for i in range(g.v):
+        if not visited[i]:
+            visited,stack=topoutil(i,g,visited,stack)
+    res=[]
+    while not len(stack)==0:
+        res.append(stack.pop())
+    return res
+
+            
+        
+    #visited=dfsutil(3,g,visited)
+def topoutil(src,g,visited,stack):
+    visited[src]=True
+    curr=g.array[src].head
+    
+    while curr:
+        if not visited[curr.dest]:
+            visited,stack=topoutil(curr.dest,g,visited,stack)
+        
+        curr=curr.next
+    stack.append(src)
+    return visited,stack
 
 
-mygraph=graph(5)
+def longestpath(g,s):
+    dist=[]
+    for i in range(g.v):
+        dist.append(-float("inf"))
+    #print dist
+    stack=topo(g)
+    dist[s]=0
+    #print dist
+    for src in stack:
+        if not dist[src]==-float("inf"):
+            crawl=g.array[src].head
+            while crawl:
+                if dist[crawl.dest]<dist[src]+crawl.weight:
+                    dist[crawl.dest]=dist[src]+crawl.weight 
+                crawl=crawl.next
+    print dist
+
+
+#mygraph=graph(5)
 #printgraph(mygraph)
-#mygraph=adduedge(mygraph,0,1)
-#mygraph=adduedge(mygraph,0,4)
-#mygraph=adduedge(mygraph,1,2)
-#mygraph=adduedge(mygraph,1,3)
-#mygraph=adduedge(mygraph,1,4)
-#mygraph=adduedge(mygraph,2,3)
-#mygraph=adduedge(mygraph,3,4)
+#mygraph=addedgeend(mygraph,0,1)
+#mygraph=addedgeend(mygraph,0,4)
+#mygraph=addedgeend(mygraph,1,2)
+#mygraph=addedgeend(mygraph,3,1)
+#mygraph=addedgeend(mygraph,1,4)
+#mygraph=addedgeend(mygraph,2,3)
+#mygraph=addedgeend(mygraph,3,4)
 #printgraph(mygraph)
 
-mygraph=adduedgeend(mygraph,0,1)
-mygraph=adduedgeend(mygraph,0,4)
-mygraph=adduedgeend(mygraph,1,2)
-mygraph=adduedgeend(mygraph,1,3)
-mygraph=adduedgeend(mygraph,1,4)
-mygraph=adduedgeend(mygraph,2,3)
-mygraph=adduedgeend(mygraph,3,4)
-printgraph(mygraph)
+#mygraph=adduedgeend(mygraph,0,1)
+#mygraph=adduedgeend(mygraph,1,4)
+#mygraph=adduedgeend(mygraph,1,2)
+#mygraph=adduedgeend(mygraph,1,3)
+#mygraph=adduedgeend(mygraph,1,4)
+#mygraph=adduedgeend(mygraph,2,3)
+#mygraph=adduedgeend(mygraph,3,4)
+#printgraph(mygraph)
+#print "BFS:"
+#bfs(mygraph,2)
+#print "\nDFS:"
+#dfs(mygraph)
+#print "\nCycle:"
+#dfscycle(mygraph)
 
-bfs(mygraph,2)
+#mygraph2=newgraph(3,2)
+#mygraph2.edgelist[0].src=0
+#mygraph2.edgelist[0].dest=1
+#mygraph2.edgelist[1].src=1
+#mygraph2.edgelist[1].dest=2
+#mygraph2.edgelist[2].src=0
+#mygraph2.edgelist[2].dest=2
+
+#print isundircycle(mygraph2)
+
+#mygraph3=graph(6)
+#printgraph(mygraph3)
+#mygraph3=addedgeend(mygraph3,5,2)
+#mygraph3=addedgeend(mygraph3,5,0)
+#mygraph3=addedgeend(mygraph3,4,0)
+#mygraph3=addedgeend(mygraph3,4,1)
+#mygraph3=addedgeend(mygraph3,2,3)
+#mygraph3=addedgeend(mygraph3,3,1)
+#mygraph3=addedgeend(mygraph3,3,4)
+#printgraph(mygraph3)
+#dfs(mygraph3)
+#print"\n"
+#topo(mygraph3)
+#x=float("inf")
+
+mygraph4=graph(6)
+printgraph(mygraph4)
+mygraph4=addedgeend(mygraph4,0,1,5)
+mygraph4=addedgeend(mygraph4,0,2,3)
+mygraph4=addedgeend(mygraph4,1,3,6)
+mygraph4=addedgeend(mygraph4,1,2,2)
+mygraph4=addedgeend(mygraph4,2,4,4)
+mygraph4=addedgeend(mygraph4,2,5,2)
+mygraph4=addedgeend(mygraph4,2,3,7)
+mygraph4=addedgeend(mygraph4,3,5,1)
+mygraph4=addedgeend(mygraph4,3,4,-1)
+mygraph4=addedgeend(mygraph4,4,5,-2)
+printgraph(mygraph4)
+print topo(mygraph4)
+
+longestpath(mygraph4,1)
--- stack.py
+++ stack.py
@@ -8,10 +8,50 @@ class stack(object):
 			return str([])
 		return str(self.arr[0:self.top+1])
 
+class twostack(object):
+	def __init__(self,capacity):
+		self.top1=-1
+		self.top2=capacity
+		self.capacity=capacity
+		self.arr=[None]*capacity
+	def __str__(self):
+		if self.top1==-1 and self.top2==self.capacity:
+			return str([])
+		return str(self.arr[0:self.top1+1])+"---"+str(self.arr[self.top2:])
+
+def push1(stack,x):
+	if stack.top1<stack.top2-1:
+		stack.top1+=1
+		stack.arr[stack.top1]=x
+	else:
+		print "O/F"
+
+def push2(stack,x):
+	if stack.top1<stack.top2-1:
+		stack.top2-=1
+		stack.arr[stack.top2]=x
+	else:
+		print "O/F"
+def pop1(stack):
+	if stack.top1>=0:
+		s=stack.arr[stack.top1]
+		stack.top1-=1
+		return s
+	else:
+		print "U/F"
+
+def pop2(stack):
+	if stack.top2<stack.capacity:
+		s=stack.arr[stack.top2]
+		stack.top2+=1
+		return s
+	else:
+		print "U/F"
+
 def isfull(stack):
 	return stack.top==stack.capacity-1
 
-def isempty(stack):
+def isemp(stack):
 	return stack.top==-1
 def push(stack,x):
 	if not isfull(stack):
@@ -20,7 +60,7 @@ def push(stack,x):
 	else:
 		print "o/f"
 def pop(stack):
-	if not isempty(stack):
+	if not isemp(stack):
 		s=stack.arr[stack.top]
 		stack.top-=1
 		return s
@@ -30,16 +70,240 @@ def pop(stack):
 
 def peek(stack):
 	return stack.arr[stack.top]
+
+def isoperand(a):
+	if (a>='a' and a<='z') or (a>='A' and a<='Z'):
+		return True
+	else :
+		return False
+def isdigit(a):
+	if (a>='0' and a<='9') :
+		return True
+	else :
+		return False
+
+def prec(a):
+	if a=='+' or a=='-':
+		return 1
+	elif a=="*" or a=="/":
+		return 2
+	elif a=="^":
+		return 3
+	else:
+		return -1
+
+def infixtoprefix(infix):
+	print infix
+	pfix=""
+	mystack=stack(len(infix))
+	for i in range(len(infix)):
+		if isoperand(infix[i]):
+			#pfix.append(infix[i])
+			pfix=pfix+infix[i]
+		elif infix[i]=='(':
+			push(mystack,infix[i])
+		elif infix[i]==')':
+			while (not isemp(mystack)) and (not peek(mystack)=='('):
+				#pfix.append(pop(mystack))
+				pfix=pfix+pop(mystack)
+			if (not isemp(mystack)) and (not peek(mystack)=='('):
+				print"Inval expression"
+				return pfix
+			pop(mystack)
+		else:
+			while (not isemp(mystack)) and prec(infix[i])<=prec(peek(mystack)):
+				#pfix.append(pop(mystack))
+				pfix=pfix+pop(mystack)
+			push(mystack,infix[i])
+
+	while not isemp(mystack):
+		#pfix.append(pop(mystack))
+		pfix=pfix+pop(mystack)
 	
+	return pfix
 
-mystack=stack(5)
+def evalpost(pfix):
+	mystack=stack(len(pfix))
+	for i in range(len(pfix)):
+		if isdigit(pfix[i]):
+			push(mystack,float(pfix[i]))
+		else:
+			a=pop(mystack)
+			b=pop(mystack)
+			if pfix[i]=='+':
+				push(mystack,b+a)
+			elif pfix[i]=='-':
+				push(mystack,b-a)
+			elif pfix[i]=='*':
+				push(mystack,b*a)
+			else :
+				push(mystack,b/a)
+
+	return pop(mystack)
+
+def revstr(inp):
+	out=""
+	mystack=stack(len(inp))
+	for i in range(len(inp)):
+		push(mystack,inp[i])
+	while (not isemp(mystack)):
+		out=out+pop(mystack)
+	return out
+
+def parencheck(inp):
+	mystack=stack(len(inp))
+	for i in range(len(inp)):
+		if inp[i]=='(' or inp[i]=='[' or inp[i]=='{':
+			push(mystack,inp[i]) 
+		else:
+			if isemp(mystack):
+				return False
+			elif inp[i]==')' and pop(mystack)=='(':
+				continue
+			elif inp[i]=='}' and pop(mystack)=='{':
+				continue
+			elif inp[i]==']' and pop(mystack)=='[':
+				continue
+			else:
+				return False
+	if isemp(mystack):
+		return True
+	else:
+		return False
+
+def nge(inp):
+	mystack=stack(len(inp))
+	push(mystack,inp[0])
+	for i in range(1,len(inp)):
+		next=inp[i]
+		if not isemp(mystack):
+			curr=pop(mystack)
+			while curr <next:
+				print str(curr)+"->"+str(next)
+				if isemp(mystack):
+					break
+				curr=pop(mystack)
+			if curr>next:
+				push(mystack,curr)
+		push(mystack,next)
+
+	while not isemp(mystack):
+		print str(pop(mystack))+"->-1"
+
+
+def insertatbot(mystack,x):
+	if isemp(mystack):
+		push(mystack,x)
+	else:
+		temp=pop(mystack)
+		insertatbot(mystack,x)
+		push(mystack,temp)
+def stackreverse(mystack):
+	if isemp(mystack):
+		return
+	temp=pop(mystack)
+	stackreverse(mystack)
+	insertatbot(mystack,temp)
+
+def stockspan(price):
+	span=[]
+	mystack=stack(len(price))
+	for i in range(len(price)):
+		span.append(0)
+	span[0]=1
+	push(mystack,0)
+	for i in range(1,len(price)):
+		curr=price[i]
+		while not isemp(mystack) and curr>price[peek(mystack)]:
+			pop(mystack)
+		if isemp(mystack):
+			span[i]=i+1
+		else:
+			span[i]=i-peek(mystack)
+		push(mystack,i)
+	return span
+
+def specialpush(mystack,spclstack,x):
+	if isemp(mystack):
+		push(mystack,x)
+		push(spclstack,x)
+	else:
+		push(mystack,x)
+		s=peek(spclstack)
+		if x<s:
+			push(spclstack,x)
+		else:
+			push(spclstack,s)
+
+def specialpop(mystack,spclstack):
+	s=pop(mystack)
+	pop(spclstack)
+	return s
+
+def getmin(spclstack):
+	return peek(spclstack)
+
+
+#mystack=stack(5)
 #print mystack
-push(mystack,1)
-push(mystack,2)
+#push(mystack,1)
+#push(mystack,2)
 
 #print mystack
 #print pop(mystack)
 #print mystack
 #print pop(mystack)
-print mystack
-print peek(mystack)
+#print mystack
+#print peek(mystack)
+
+#print infixtoprefix("a+b*(c^d-e)^(f+g*h)-i")
+#print evalpost("8231*+9-/")
+
+#print revstr("MANISH")
+
+#mystack2=twostack(10)
+#print mystack2
+#push1(mystack2,10)
+#push1(mystack2,20)
+#ush1(mystack2,15)
+#push2(mystack2,1)
+#push2(mystack2,19)
+#print mystack2
+#print pop1(mystack2)
+#print pop2(mystack2)
+#print pop2(mystack2)
+#print mystack2
+
+#print parencheck("[(({[]})])")
+
+#arr1=[4,2,5,20,2]
+#nge(arr1)
+
+#mystack=stack(5)
+#push(mystack,2)
+#push(mystack,3)
+#print mystack
+#insertatbot(mystack,4)
+#print mystack
+#stackreverse(mystack)
+#print mystack
+
+#print stockspan([10, 4, 5, 90, 120, 80])
+
+#mystack=stack(5)
+#spclstack=stack(5)
+#print mystack,spclstack
+#specialpush(mystack,spclstack,5)
+#print mystack,spclstack
+#specialpush(mystack,spclstack,10)
+#print mystack,spclstack
+#specialpush(mystack,spclstack,2)
+#print mystack,spclstack
+#specialpush(mystack,spclstack,1)
+#print mystack,spclstack
+#print specialpop(mystack,spclstack)
+#print mystack,spclstack
+#print specialpop(mystack,spclstack)
+#print mystack,spclstack
+
+
